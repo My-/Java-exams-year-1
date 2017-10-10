905bef0e2b408cb98c45e14d370a8601e839823e
My- <minde.pinde+my-@gmail.com>
2017-10-10 17:25:55 +0100

    added starter methods

---
+++ Exam-4/Year-2016/InvestmentGuaranteed.java
@@ -0,0 +1,12 @@
+// ClassName - your name here
+
+public class InvestmentGuaranteed
+{
+//===  M e m b e r   V a r i a b l e s   ============================
+
+
+//===  M e m b e r   M e t h o d s  =================================
+
+
+//===================================================================
+} // InvestmentGuaranteed
